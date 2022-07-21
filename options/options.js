540d275f9301905566234df708ea966f96bbde68
Crowdbotics <team@crowdbotics.com>
2022-07-21 18:13:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "medigrill_36336",
+  url: "https://medigrill_36336.botics.co",
+  api: "https://medigrill_36336.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
